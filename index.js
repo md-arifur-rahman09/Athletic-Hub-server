8ea958c996593b68c9c93a91589be9bd2f4cdf06
md arifur rahman <aimless.arif09@gmail.com>
2025-06-14 23:18:03 +0600

    jwt token generate and verify token and verify email done

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 require('dotenv').config();
 const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, MongoExpiredSessionError } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const { populate } = require('dotenv');
+
 
 const port = process.env.port || 3000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
 
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        };
+        req.decoded = decoded;
 
+    })
+
+    next();
+}
+
+
+const verifyEmailToken = (req, res, next) => {
+    if (req.query.email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+    };
+    next();
+}
 
 
 
@@ -46,6 +71,22 @@ async function run() {
         const eventsCollection = client.db('athleticHubDB').collection('events');
         const bookingsCollection = client.db('athleticHubDB').collection('bookings')
 
+
+
+        // JWT jot token
+        app.post('/jwt', async (req, res) => {
+            const email = req.body;
+            const token = jwt.sign(email, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false
+            })
+            res.send({ success: true });
+
+        })
+
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
         app.get('/allEvents', async (req, res) => {
             const result = await eventsCollection.find().toArray();
             res.send(result);
@@ -59,36 +100,40 @@ async function run() {
         });
 
         // my posted events
-        app.get('/events', async (req, res) => {
+        app.get('/events', verifyToken, verifyEmailToken, async (req, res) => {
             const email = req.query.email;
             const query = { hr_email: email };
             const result = await eventsCollection.find(query).toArray();
             res.send(result);
-        })
+        });
 
         // add event by HR or ADMIN
         app.post('/allEvents', async (req, res) => {
             const eventInfo = req.body;
             const result = await eventsCollection.insertOne(eventInfo);
             res.send(result)
-        })
+        });
 
+
+        // update event by HR/ADMIN
         app.put('/events/:id', async (req, res) => {
             const updatedEvent = req.body;
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
-                $set: 
+                $set:
                     updatedEvent
-                
+
             };
             const options = { upsert: true };
             const result = await eventsCollection.updateOne(filter, updatedDoc, options);
 
             res.send(result);
 
-        })
+        });
 
+
+        // delete event by HR/ADMIN
         app.delete('/events/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -97,13 +142,13 @@ async function run() {
         })
 
 
-        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
 
         //  BOOKINGs
 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyToken, verifyEmailToken, async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
             const bookings = await bookingsCollection.find(query).toArray();
@@ -118,12 +163,12 @@ async function run() {
                 booking.eventName = event.eventName;
                 booking.image = event.image;
 
-
             }
             res.send(bookings);
         })
-        //book event 
 
+
+        //book event 
         app.post('/bookings', async (req, res) => {
             const eventBookingData = req.body;
             const result = await bookingsCollection.insertOne(eventBookingData)
@@ -139,6 +184,14 @@ async function run() {
             res.send(result);
         })
 
+
+
+        app.get('/events/:id',verifyToken, async (req, res) => {
+            const id = req.params.id;
+            const query = { eventId: id }
+            const result = await bookingsCollection.find(query).toArray();
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
